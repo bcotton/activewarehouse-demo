681951dd08014e7f0d4a6335b0e2c5651c9e279d
aeden <aeden@78b0de1e-7a20-0410-a083-82450d3afd0a>
2006-11-07 01:07:20 +0000

    Fixes to demo. Added fact list
    git-svn-id: svn://rubyforge.org/var/svn/activewarehouse/demo/trunk@47 78b0de1e-7a20-0410-a083-82450d3afd0a

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,13 @@
 class DemoController < ApplicationController
 
   def index
+    render :action => "demo/index", :layout => "application"
+  end
+  def example1
+    
+  end
+  def fact_list
+    @facts = PosRetailSalesTransactionFact.find(
+      :all, :include => [:date, :store])
   end
 end
--- app/models/pos_retail_sales_transaction_fact.rb
+++ app/models/pos_retail_sales_transaction_fact.rb
@@ -1,4 +1,6 @@
 class PosRetailSalesTransactionFact < ActiveWarehouse::Fact
   define_aggregate :sales_quantity, :type => :sum
   define_aggregate :sales_dollar_amount, :type => :sum
+  belongs_to :date, :foreign_key => 'date_id', :class_name => 'DateDimension'
+  belongs_to :store, :foreign_key => 'store_id', :class_name => 'StoreDimension'
 end
