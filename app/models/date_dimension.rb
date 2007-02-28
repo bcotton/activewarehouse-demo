dd4db2bcc34fdca447789f2e67bdf3fa9e1dfb66
aeden <aeden@78b0de1e-7a20-0410-a083-82450d3afd0a>
2007-02-28 18:54:43 +0000

    Set the order in the date dimension
    git-svn-id: svn://rubyforge.org/var/svn/activewarehouse/demo/trunk@334 78b0de1e-7a20-0410-a083-82450d3afd0a

--- app/models/date_dimension.rb
+++ app/models/date_dimension.rb
@@ -1,4 +1,5 @@
 class DateDimension < ActiveWarehouse::Dimension
+  set_order :id
   define_hierarchy :cy, [:calendar_year,:calendar_quarter,:calendar_month_name,:calendar_week,:day_of_week]
   define_hierarchy :fy, [:fiscal_year,:fiscal_quarter,:calendar_month_name,:fiscal_week,:day_of_week]
   define_hierarchy :calendar_weeks, [:calendar_week]
