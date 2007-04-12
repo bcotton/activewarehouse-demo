5bb452cace198dc672bf4933de403431b6631670
aeden <aeden@78b0de1e-7a20-0410-a083-82450d3afd0a>
2007-04-12 21:23:33 +0000

    Converted belongs_to to dimension directive
    git-svn-id: svn://rubyforge.org/var/svn/activewarehouse/demo/trunk@582 78b0de1e-7a20-0410-a083-82450d3afd0a

--- app/models/pos_retail_sales_transaction_fact.rb
+++ app/models/pos_retail_sales_transaction_fact.rb
@@ -5,8 +5,8 @@ class PosRetailSalesTransactionFact < ActiveWarehouse::Fact
   
   calculated_field (:gross_margin) { |agg_rec| agg_rec.gross_profit_dollar_amount / agg_rec.sales_dollar_amount}
   
-  belongs_to :date, :foreign_key => 'date_id', :class_name => 'DateDimension'
-  belongs_to :store, :foreign_key => 'store_id', :class_name => 'StoreDimension'
-  belongs_to :product, :foreign_key => 'product_id', :class_name => 'ProductDimension'
-  belongs_to :promotion, :foreign_key => 'promotion_id', :class_name => 'PromotionDimension'
+  dimension :date
+  dimension :store
+  dimension :product
+  dimension :promotion
 end
